64aa683562aed61ee8468864bf6da1e695082714
Devilal panchal <devilalpanchal551@gmail.com>
2024-03-01 18:52:26 +0530

    add

---
+++ index.js
@@ -0,0 +1,7 @@
+////// For get current location and go to other location
+
+
+alert(location.href); 
+if (confirm("Go to GithubProfile of Devid")) {
+  location.href = "https://github.com/devilalpanchal"; // redirect the browser to another URL
+}
